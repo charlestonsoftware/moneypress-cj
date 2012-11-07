a7b946fdb34da662a5d857e0f978e1db75843310
Lance Cleveland <lance@cybersprocket.com>
2012-11-06 22:11:52 -0500

    Consolidate plus into helper.

--- include/csl_helpers.php
+++ include/csl_helpers.php
@@ -426,3 +426,85 @@ function wpCJ_url_exists($url) {
   return $connectable;
 }
 
+//////////// FUNCTIONS
+
+// Not being used currently, will require heavy reworking to interface with Wordpress properly
+function wpCJ_keyword_list($keywords) {
+  foreach ($keywords as $keyword=>$count) {
+    $keyword_path = ((isset($search_keywords)) ? str_replace(' ', '/', $search_keywords) : '') . "/";
+    // Not sure whether or not the links should be addative at this point...
+    // echo "<a href='" . ROOT_POSTFIX . "/$keyword_path$keyword'>$keyword</a> \n";
+    echo "<a href='" . ROOT_POSTFIX . "/$keyword'>$keyword</a> \n";
+  }
+}
+
+function wpCJ_Handle_Admin_Menu() {
+  global $MP_cj_plugin;
+
+  if ($MP_cj_plugin->settings->check_required('CJ Communications')) {
+    add_meta_box('wpcjStoreMB', 'CSL Quick Commission Junction Entry', 'wpCJ_StoreInsertForm', 'post', 'normal');
+    add_meta_box('wpcjStoreMB', 'CSL Quick Commission Junction Entry', 'wpCJ_StoreInsertForm', 'page', 'normal');
+  }
+}
+
+function wpCJ_Admin_Head () {
+  if ($GLOBALS['editing']) {
+    wp_enqueue_script('wpCJStoreAdmin', plugins_url('js/cjstore.js', __FILE__), array('jquery'), '1.0.0');
+  }
+}
+
+function wpCJ_StoreInsertForm() {
+?>
+<table class="form-table">
+  <tr valign="top">
+    <th align="right" scope="row"><label for="wpCJ_keywords"><?php _e('Keywords:')?></label></th>
+    <td>
+      <input type="text" size="40" style="width:95%;" name="wpCJ_keywords" id="wpCJ_keywords" />
+    </td>
+  </tr>
+  <tr valign="top">
+    <th align="right" scope="row"><label for="wpCJ_itemcount"><?php _e('Number of Items:')?></label></th>
+    <td>
+      <select name="wpCJ_itemcount" id="wpCJ_itemcount">
+        <option>1</option>
+        <option>5</option>
+        <option>10</option>
+        <option>20</option>
+        <option>50</option>
+      </select>
+    </td>
+  </tr>
+</table>
+<p class="submit">
+  <input type="button" onclick="return this_wpCJAdmin.sendToEditor(this.form);" value="<?php _e('Create Commission Junction Shortcode &raquo;'); ?>" />
+</p>
+<?php
+}
+
+/**************************************
+ ** function: csl_mpcj_add_settings()
+ **
+ ** Add plus settings to the admin interface.
+ **
+ **/
+function csl_mpcj_add_settings() {
+    global $MP_cj_plugin;
+
+    $MP_cj_plugin->settings->add_section(
+        array(
+            'name'          => __('Display Settings',MP_CJ_PREFIX),
+            'description'   => ''
+        )
+    );
+
+    $MP_cj_plugin->settings->add_item(
+        'Display Settings',
+        __('Link Modifiers',MP_CJ_PREFIX),
+        'link_modifiers',
+        'text',
+        false,
+        __('Enter any modifiers you want to add to the external product link. ' .
+        'Must be a fully formed anchor (A tag) qualifier.  Example: rel="nofollow"',MP_CJ_PREFIX)
+    );
+}
+
--- mp-cj.php
+++ mp-cj.php
@@ -63,9 +63,10 @@ if (defined('MP_CJ_ICONURL') === false) {
 
 // Include our needed files
 //
+global $MP_cj_plugin;
 include_once(MP_CJ_PLUGINDIR . '/include/config.php'   );
-include_once(MP_CJ_PLUGINDIR . '/include/csl_helpers.php'       );
-include_once(MP_CJ_PLUGINDIR . 'plus.php'       );
+include_once(MP_CJ_PLUGINDIR . '/include/csl_helpers.php');
+
 if (class_exists('PanhandlerProduct') === false) {
     try {
         require_once('Panhandler/Panhandler.php');
@@ -85,80 +86,19 @@ if (class_exists('CommissionJunctionPanhandler') === false) {
     }
 }
 
-
-
-
 register_activation_hook( __FILE__, 'csl_mpcj_activate');
 
-add_action('wp_footer', 'csl_mpcj_user_stylesheet');
+// actions
 add_action('admin_print_styles','csl_mpcj_admin_stylesheet');
 add_action('admin_init','csl_mpcj_setup_admin_interface',10);
+add_action('wp_footer', 'csl_mpcj_user_stylesheet');
                          
-
 //-----------------------------------------------------------------------------
 // LEGACY STUFF - CAN PROBABLY GO AWAY
 //-----------------------------------------------------------------------------
 if ( is_admin() ) {
   add_action('admin_menu', 'wpCJ_Handle_Admin_Menu');
   add_filter('admin_print_scripts', 'wpCJ_Admin_Head');
-} else {
-  // non-admin enqueues, actions, and filters
 }
 
 
-//////////// FUNCTIONS
-
-// Not being used currently, will require heavy reworking to interface with Wordpress properly
-function wpCJ_keyword_list($keywords) {
-  foreach ($keywords as $keyword=>$count) {
-    $keyword_path = ((isset($search_keywords)) ? str_replace(' ', '/', $search_keywords) : '') . "/";
-    // Not sure whether or not the links should be addative at this point...
-    // echo "<a href='" . ROOT_POSTFIX . "/$keyword_path$keyword'>$keyword</a> \n";
-    echo "<a href='" . ROOT_POSTFIX . "/$keyword'>$keyword</a> \n";
-  }
-}
-
-function wpCJ_Handle_Admin_Menu() {
-  global $MP_cj_plugin;
-
-  if ($MP_cj_plugin->settings->check_required('CJ Communications')) {
-    add_meta_box('wpcjStoreMB', 'CSL Quick Commission Junction Entry', 'wpCJ_StoreInsertForm', 'post', 'normal');
-    add_meta_box('wpcjStoreMB', 'CSL Quick Commission Junction Entry', 'wpCJ_StoreInsertForm', 'page', 'normal');
-  }
-}
-
-function wpCJ_Admin_Head () {
-  if ($GLOBALS['editing']) {
-    wp_enqueue_script('wpCJStoreAdmin', plugins_url('js/cjstore.js', __FILE__), array('jquery'), '1.0.0');
-  }
-}
-
-function wpCJ_StoreInsertForm() {
-?>
-<table class="form-table">
-  <tr valign="top">
-    <th align="right" scope="row"><label for="wpCJ_keywords"><?php _e('Keywords:')?></label></th>
-    <td>
-      <input type="text" size="40" style="width:95%;" name="wpCJ_keywords" id="wpCJ_keywords" />
-    </td>
-  </tr>
-  <tr valign="top">
-    <th align="right" scope="row"><label for="wpCJ_itemcount"><?php _e('Number of Items:')?></label></th>
-    <td>
-      <select name="wpCJ_itemcount" id="wpCJ_itemcount">
-        <option>1</option>
-        <option>5</option>
-        <option>10</option>
-        <option>20</option>
-        <option>50</option>
-      </select>
-    </td>
-  </tr>
-</table>
-<p class="submit">
-  <input type="button" onclick="return this_wpCJAdmin.sendToEditor(this.form);" value="<?php _e('Create Commission Junction Shortcode &raquo;'); ?>" />
-</p>
-<?php
-}
-
-?>
